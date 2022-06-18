76ae39eac319c3738d9eda184b88de3cdf85f364
emrsyhn <emirseyhana.z@gmail.com>
2022-06-18 17:38:15 +0300

    tasarım oluyor gibi

--- lib/userpage.dart
+++ lib/userpage.dart
@@ -44,48 +44,134 @@ class _UserPageState extends State<UserPage> {
                       onPressed: () {},
                       icon: const Icon(Icons.logout),
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text(
-                          'Selam Mübarek\n Kullanıcı',
-                          style: TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.bold),
-                        ),
-                        Image.asset(
-                          'assets/images/1.png',
-                          height: 60,
-                          width: 60,
-                        ),
-                      ],
+                    Padding(
+                      padding: const EdgeInsets.only(right: 9, bottom: 9),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            'Selam Mübarek\n Kullanıcı',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Image.asset(
+                            'assets/images/User.png',
+                            height: 60,
+                            width: 60,
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
               ),
-              Container(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    const Text(
-                      'Kitap Ara',
-                      style: const TextStyle(color: Colors.green),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        AnimSearchBar(
-                          helpText: 'Arama yap',
-                          width: 300,
-                          textController: textController,
-                          onSuffixTap: () {
-                            setState(() {
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  const Text(
+                    'Kitap Ara',
+                    style: TextStyle(color: Colors.green),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      AnimSearchBar(
+                        helpText: 'Arama yap',
+                        width: 300,
+                        textController: textController,
+                        onSuffixTap: () {
+                          setState(
+                            () {
                               textController.clear();
-                            });
-                          },
-                        ),
-                      ],
-                    ),
-                  ],
+                            },
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(height: 5),
+              Container(
+                color: const Color.fromARGB(255, 152, 150, 150),
+                width: MediaQuery.of(context).size.width,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(left: 10),
+                        child: Text('Çok okunan kitaplar'),
+                      ),
+                      Row(
+                        children: [
+                          IconButton(
+                            icon: Image.asset('assets/images/kitap.png'),
+                            iconSize: 100,
+                            onPressed: () {},
+                          ),
+                          IconButton(
+                            icon: Image.asset('assets/images/kitap2.png'),
+                            iconSize: 100,
+                            onPressed: () {},
+                          ),
+                          IconButton(
+                            icon: Image.asset('assets/images/kitap3.png'),
+                            iconSize: 100,
+                            onPressed: () {},
+                          ),
+                          IconButton(
+                            icon: Image.asset('assets/images/kitap4.png'),
+                            iconSize: 100,
+                            onPressed: () {},
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                color: const Color.fromARGB(255, 152, 150, 150),
+                height: MediaQuery.of(context).size.height * 0.003,
+              ),
+              const SizedBox(height: 10),
+              Container(
+                color: Colors.amber,
+                width: MediaQuery.of(context).size.width,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          IconButton(
+                            icon: Image.asset('assets/images/kelaynak.png'),
+                            iconSize: 100,
+                            onPressed: () {},
+                          ),
+                          const Text(
+                            'Ustad amcık\n Programı 19:00"da',
+                            textAlign: TextAlign.center,
+                          ),
+                          IconButton(
+                            icon: Image.asset('assets/images/kelaynak.png'),
+                            iconSize: 100,
+                            onPressed: () {},
+                            tooltip: 'button with icon',
+                            highlightColor: Colors.deepOrangeAccent,
+                          ),
+                          const Text(
+                            'Ustad amcık\n Programı 19:00"da',
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
